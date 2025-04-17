9c6fcc64e37c16bc6e9a0ccab1d9d43d5c2bd3cd
Marcos <devmarcos7@gmail.com>
2025-04-17 11:13:51 -0300

    condional para mostrar botoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import Formulario from "./components/Formulario";
 import Tabela from "./components/Tabela";
 
 function App() {
+
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+
   return (
     <>
-      <Formulario />
+      <Formulario btnCadastrar={btnCadastrar} />
       <Tabela />
     </>
   );
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,19 +1,23 @@
-function Formulario(){
+function Formulario({ btnCadastrar }) {
+  return (
+    <>
+      <form>
+        <input type="text" placeholder="Nome" className="form-control " />
+        <input type="email" placeholder="E-mail" className="form-control " />
+        <input type="text" placeholder="Cidade" className="form-control " />
 
-    return (
-      <>
-        <form>
-          <input type="text" placeholder="Nome" className="form-control " />
-          <input type="email" placeholder="E-mail" className="form-control " />
-          <input type="text" placeholder="Cidade" className="form-control " />
-
+        {btnCadastrar ? (
           <input type="button" value="Cadastrar" className="btn btn-primary" />
-          <input type="button" value="Alterar" className="btn btn-secondary" />
-          <input type="button" value="Excluir" className="btn btn-danger" />
-          <input type="button" value="Cancelar" className="btn btn-success" />
-        </form>
-      </>
-    );
+        ) : (
+          <>
+            <input type="button" value="Alterar" className="btn btn-secondary" />
+            <input type="button" value="Excluir" className="btn btn-danger" />
+            <input type="button" value="Cancelar" className="btn btn-success" />
+          </>
+        )}
+      </form>
+    </>
+  );
 }
 
 export default Formulario;
