6e4ac6ea8ac19d04be3c78eb94de3e9c844e5eb3
Marcos <devmarcos7@gmail.com>
2025-04-18 08:51:44 -0300

    pesquisa por nome finalizada

--- src/components/Tabela.jsx
+++ src/components/Tabela.jsx
@@ -1,35 +1,47 @@
-function Tabela({ vetor, selecionar }) {
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Indice</th>
-          <th>Nome</th>
-          <th>Idade</th>
-          <th>Cidade</th>
-          <th>Selecionar</th>
-        </tr>
-      </thead>
+import { useState } from "react";
 
-      <tbody>
-        {vetor.map((obj, indice) => (
-          <tr key={indice}>
-            <td>{indice + 1}</td>
-            <td>{obj.nome}</td>
-            <td>{obj.idade}</td>
-            <td>{obj.cidade}</td>
-            <td>
-              <button
-                className="btn btn-success"
-                onClick={() => selecionar(indice)}
-              >
-                Selecionar
-              </button>
-            </td>
+function Tabela({ vetor, selecionar }) {
+  const [termo, setTermo] = useState("");
+
+  return (
+    <>
+      <input
+        type="text"
+        onChange={(e) => setTermo(e.target.value)}
+        placeholder="Informe o nome"
+        className="form-control pesquisar"
+      />
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Indice</th>
+            <th>Nome</th>
+            <th>Idade</th>
+            <th>Cidade</th>
+            <th>Selecionar</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+          {vetor.filter(obj => obj.nome.includes(termo)).map((obj, indice) => (
+            <tr key={indice}>
+              <td>{indice + 1}</td>
+              <td>{obj.nome}</td>
+              <td>{obj.idade}</td>
+              <td>{obj.cidade}</td>
+              <td>
+                <button
+                  className="btn btn-success"
+                  onClick={() => selecionar(indice)}
+                >
+                  Selecionar
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
